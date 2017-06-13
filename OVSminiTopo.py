151ec88778cb0d77e6c55c81865dadb36d0255fc
zohebys <zohebys@iitb,ac,in>
2017-06-12 23:47:17 -0700

    Add data flows and tcpdump

--- OVSminiTopo.py
+++ OVSminiTopo.py
@@ -6,62 +6,7 @@ from mininet.link import TCLink, Intf
 from mininet.log import setLogLevel
 import logging as log
 from configs import *
-
-client_iperfs=[]
-server_iperfs=[]
-net = Mininet(topo=None,
-                      build=False,
-                      ipBase='10.0.0.0/8',
-                      autoSetMacs=True,
-                      )
-def _get_mininet_nodes(nodes):
-        """
-        Choose the actual Mininet Hosts (rather than just strings) that will
-        be subscribers.
-        :param List[str] nodes:
-        :return List[Node] mininet_nodes:
-        """
-        return [net.get(n) for n in nodes]
-
-def setup_traffic_generators():
-        """Each traffic generating host starts an iperf process aimed at
-        (one of) the server(s) in order to generate random traffic and create
-        congestion in the experiment.  Traffic is all UDP because it sets the bandwidth.
-
-        NOTE: iperf v2 added the capability to tell the server when to exit after some time.
-        However, we explicitly terminate the server anyway to avoid incompatibility issues."""
-
-	internet_hosts=['h3','h6','h9','h12']
-	stream_hosts=['h4','h7','h10','h13']
-
-        internet_srv = net.getNodeByName('h2')
-	edge_srv = net.getNodeByName('h1')
-
-	generators = _get_mininet_nodes(internet_hosts)
-        log.info("*** Starting background traffic generators for internet")
-        # We enumerate the generators to fill the range of ports so that the server
-        # can listen for each iperf client.
-        for n, g in enumerate(generators):
-            log.info("iperf from %s to %s" % (g, internet_srv))
-            # can't do self.net.iperf([g,s]) as there's no option to put it in the background
-            i = g.popen('iperf -p %d -t %d -c %s &' % (IPERF_INT_BASE_PORT + n, EXPERIMENT_DURATION, internet_srv.IP()))
-            client_iperfs.append(i)
-            i = internet_srv.popen('iperf -p %d -t %d -s &' % (IPERF_INT_BASE_PORT + n, EXPERIMENT_DURATION))
-            server_iperfs.append(i)
-
-	generators = _get_mininet_nodes(stream_hosts)
-
-        log.info("*** Starting background traffic generators for streaming")
-        # We enumerate the generators to fill the range of ports so that the server
-        # can listen for each iperf client.
-        for n, g in enumerate(generators):
-            log.info("iperf from %s to %s" % (g, edge_srv))
-            # can't do self.net.iperf([g,s]) as there's no option to put it in the background
-            i = g.popen('iperf -p %d -t %d -u -b %dM -c %s &' % (IPERF_ST_BASE_PORT + n, EXPERIMENT_DURATION, ST_BAND, edge_srv.IP()))
-            client_iperfs.append(i)
-            i = edge_srv.popen('iperf -p %d -t %d -u -s &' % (IPERF_ST_BASE_PORT + n, EXPERIMENT_DURATION))
-            server_iperfs.append(i)
-
+import data_flows as df
 
 def setup_topology():
         """
@@ -101,7 +46,8 @@ def setup_topology():
             hosts.append(h)
 
         manual_links = [['s1', 'h1', 100], ['s1', 'h2', 100], ['s1', 's2', 50 ],
-                        ['s1', 's3', 50 ], ['s2', 's3', 50 ], ['s2', 's4', 50 ],
+                        ['s1', 's3', 50 ], #['s2', 's3', 50 ], 
+			['s2', 's4', 50 ],
                         ['s2', 's5', 50 ], ['s3', 's6', 50 ], ['s3', 's7', 50 ],
                         ['s4', 'h3', 10 ], ['s4', 'h4', 10 ], ['s4', 'h5', 10 ],
                         ['s5', 'h6', 10 ], ['s5', 'h7', 10 ], ['s5', 'h8', 10 ],
@@ -169,8 +115,8 @@ def setup_topology():
 
     	#net.pingAll()
     	#Starting the internet and stream traffic
-    	#setup_traffic_generators()
-
+    	df.setup_traffic_generators(net)
+	#Popen("killall -9 top bwm-ng", shell=True).wait()
         # Drop the user in to a CLI so user can run commands.
         CLI( net )
 
@@ -182,6 +128,11 @@ def setup_topology():
 
 if __name__ == '__main__':
     # This runs if this file is executed directly
+    net = Mininet(topo=None,
+                      build=False,
+                      ipBase='10.0.0.0/8',
+                      autoSetMacs=True,
+                      )
     setLogLevel( 'info' )
     setup_topology()
 
---
+++ data_flows.py
@@ -0,0 +1,87 @@
+from mininet.net import Mininet
+from mininet.node import RemoteController, Host, OVSKernelSwitch, Controller
+from mininet.node import Switch, Link, Node, OVSController, DefaultController
+from mininet.cli import CLI
+from mininet.link import TCLink, Intf
+from mininet.log import setLogLevel
+import logging as log
+from OVSminiTopo import *
+from configs import *
+from time import sleep, time
+import sys
+import os
+client_iperfs=[]
+server_iperfs=[]
+
+def _get_mininet_nodes(mininet,nodes):
+        """
+        Choose the actual Mininet Hosts (rather than just strings) that will
+        be subscribers.
+        :param List[str] nodes:
+        :return List[Node] mininet_nodes:
+        """
+        return [mininet.get(n) for n in nodes]
+def progress(t):
+    while t > 0:
+        print('  %3d seconds left  \r' % (t))
+        t -= 1
+        sys.stdout.flush()
+        sleep(1)
+    print '\r\n'
+
+
+def setup_traffic_generators(mininet):
+        """Each traffic generating host starts an iperf process aimed at
+        (one of) the server(s) in order to generate random traffic and create
+        congestion in the experiment.  Traffic is all UDP because it sets the bandwidth.
+
+        NOTE: iperf v2 added the capability to tell the server when to exit after some time.
+        However, we explicitly terminate the server anyway to avoid incompatibility issues."""
+
+	internet_hosts=['h3','h6','h9','h12']
+	stream_hosts=['h4','h7','h10','h13']
+
+        internet_srv = mininet.getNodeByName('h2')
+	edge_srv = mininet.getNodeByName('h1')
+
+	switches=['s1','s2','s3','s4','s5','s6','s7']
+	ports=['eth0','eth1','eth2','eth3']
+
+	for s in switches:
+	    sw=mininet.getNodeByName(s)
+	    for p in ports:
+	        tmp=s+"-"+p
+		fn="/home/onos/iotDynamicPri/Results/"+tmp+".pcap"
+		sw.popen('tcpdump -i %s -s 65535 -w %s'% (tmp, fn), shell=True)
+
+	generators = _get_mininet_nodes(mininet, internet_hosts)
+        log.info("*** Starting background traffic generators for internet")
+        # We enumerate the generators to fill the range of ports so that the server
+        # can listen for each iperf client.
+        for n, g in enumerate(generators):
+            log.info("iperf from %s to %s" % (g, internet_srv))
+            # can't do self.net.iperf([g,s]) as there's no option to put it in the background
+            i = internet_srv.popen('iperf3 -p %d -s > /home/onos/iotDynamicPri/Results/Internet/%s_%s.txt &' % (IPERF_INT_BASE_PORT + n, internet_srv,g), shell=True)
+            server_iperfs.append(i)
+            i = g.popen('iperf3 -p %d -t %d -c %s -i %d > /home/onos/iotDynamicPri/Results/Internet/%s.txt &' % (IPERF_INT_BASE_PORT + n, EXPERIMENT_DURATION, internet_srv.IP(), VER, g), shell=True)
+            client_iperfs.append(i)
+
+	
+	generators = _get_mininet_nodes(mininet, stream_hosts)
+        log.info("*** Starting background traffic generators for streaming")
+        # We enumerate the generators to fill the range of ports so that the server
+        # can listen for each iperf client.
+        for n, g in enumerate(generators):
+            log.info("iperf from %s to %s" % (g, edge_srv))
+            # can't do self.net.iperf([g,s]) as there's no option to put it in the background
+	    print('iperf3 -p %d -t %d -u -b %dM -c %s -i %d > /home/onos/iotDynamicPri/Results/Stream/%s.txt &' % (IPERF_ST_BASE_PORT + n, EXPERIMENT_DURATION, ST_BAND, edge_srv.IP(), VER, g))
+	    print('iperf3 -p %d -s > /home/onos/iotDynamicPri/Results/Stream/%s_%s.txt &' % (IPERF_ST_BASE_PORT + n, edge_srv, g))
+            i = edge_srv.popen('iperf3 -p %d -s > /home/onos/iotDynamicPri/Results/Stream/%s_%s.txt &' % (IPERF_ST_BASE_PORT + n, edge_srv, g), shell=True)
+	    print(i)
+            server_iperfs.append(i)
+	    i = g.popen('iperf3 -p %d -t %d -u -b %dM -c %s -i %d > /home/onos/iotDynamicPri/Results/Stream/%s.txt &' % (IPERF_ST_BASE_PORT + n, EXPERIMENT_DURATION, ST_BAND, edge_srv.IP(), VER, g), shell=True)
+            client_iperfs.append(i)
+	progress(EXPERIMENT_DURATION+5)
+	os.system('pkill iperf3')
+	os.system('pkill tcpdump')
+
